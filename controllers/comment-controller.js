5d8057d9d61be9ea96fd3a5b1b193a9fd2699a29
St4d3ntG4zm4n <bwonder91@live.com>
2022-03-16 21:06:37 -0700

    adding the editing in it

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -13,6 +13,7 @@ const commentController = {
                 );
             })
             .then(dbPizzaData => {
+                console.log(dbPizzaData);
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
                     return;
@@ -24,7 +25,11 @@ const commentController = {
 
     // add reply to comment
     addReply({ params, body }, res) {
-        Comment.findOneAndUpdate({ _id: params.commentId }, { $push: { replies: body } }, { new: true })
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true, runValidators: true }
+            )
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
             res.status(404).json({ message: 'No pizza found with this id!' });
